66e51d59a6998c890f046ed01a0c7e9fe01b3814
poiq <bloodbornps@mail.ru>
2022-03-20 17:09:06 +0700

    generate table

--- src/script.js
+++ src/script.js
@@ -1,18 +1,30 @@
-let funk = () => {
-    return new Promise((resolve, reject) => {
-        let count = +prompt('enter number 1 or 0', 0);
-        if (count == 1) {
-            resolve(count);
-        } else if (count == 0) {
-            reject(count);
-        } else {
-            reject(count);
-        }
-    }).then(count => {
-        console.log(`its then ${count}`);
-    }).catch(count => {
-        console.log(`its catch ${count}`);
-    })
-}
+// создание тегов table thead tbody
+// связывание тегов с body 
 
-funk();
+const table = document.createElement('table');
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody');
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+document.getElementById('body').appendChild(table);
+
+const row_1 = document.createElement('tr');
+const row_1_data_1 = document.createElement('tr');
+
+let colomn = +prompt("enter colomt", 5);
+let line = prompt("enter colomt", 5);
+
+for (let c = 0; c < colomn; c++){
+    let row = document.createElement('tr');
+    row.id = `colomnum${c}`
+    for (let l = 0; l < line; l++) {
+        let row_child = document.createElement('td');
+        row_child.id = `line${c}${l}`;
+        row_child.className = 'linestyle';
+        row_child.innerHTML = c + l;
+        row.appendChild(row_child);
+    }
+    tbody.appendChild(row);
+}
